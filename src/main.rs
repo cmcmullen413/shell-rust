5eba48d6c58715db0dfce14b40402a87fb4f0550
Caleb McMullen <cmcmullen@gmail.com>
2026-06-28 20:41:45 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,20 @@
 use std::io::{self, Write};
 
 fn main() {
-    // Print the prompt
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    // Begin looping
+    loop {
+        // Print the prompt
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    // Read the user's input into a buffer
-    let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer).unwrap();
-    // Remove the tailing new line char  (or chars in case of windows)
-    let buffer = buffer.trim_end();
+        // Read the user's input into a buffer
+        let mut buffer = String::new();
+        io::stdin().read_line(&mut buffer).unwrap();
+        // Remove the tailing new line char  (or chars in case of windows)
+        let buffer = buffer.trim_end();
 
-    // Print it back out in the error message
-    // Should be in the format {command}: command not found
-    println!("{}: command not found", buffer);
+        // Print it back out in the error message
+        // Should be in the format {command}: command not found
+        println!("{}: command not found", buffer);
+    }
 }
