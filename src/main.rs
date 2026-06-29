c576903a4764e0d3569eb701091a587039b172bf
Caleb McMullen <cmcmullen@gmail.com>
2026-06-28 20:48:38 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,12 @@ fn main() {
         // Remove the tailing new line char  (or chars in case of windows)
         let buffer = buffer.trim_end();
 
+        // If the command is "exit", break the loop
+        if buffer == "exit" {
+            break;
+        }
+
+        // If the command is not recognized:
         // Print it back out in the error message
         // Should be in the format {command}: command not found
         println!("{}: command not found", buffer);
